79f2e352538d350f564a522716c33b408e534c1e
sifusan <fuglestad.simen@gmail.com>
2016-03-22 04:04:45 +0100

    fixed faulty commit, previous contained unfinished work

--- ui_controller.py
+++ ui_controller.py
@@ -1,14 +1,22 @@
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.app import App
 from kivy.lang import Builder
+from kivy.app import App
 
-
+#need to add a fail-safe here to avoid adding multiple menus with
+#the same name
 Builder.load_file("homescreen.kv")
 Builder.load_file("joinscreen.kv")
 
+class HomeScreen(Screen):
+    pass
+
+
+class JoinScreen(Screen):
+    pass
+
 sm = ScreenManager(transition=NoTransition())
 home = HomeScreen(name='home')
-join = OtherScreen(name='join')
+join = JoinScreen(name='join')
 sm.add_widget(home)
 sm.add_widget(join)
 
@@ -16,5 +24,5 @@ class MyApp(App):
     def build(self):
         return sm
 
-if __name__ == '__main__':
+if __name__=='__main__':
     MyApp().run()
