d2bb08f6b017be7c5284707acef4d579e074dbe3
sifusan <fuglestad.simen@gmail.com>
2016-04-26 14:48:11 +0200

    changed back to using .py files for handling ui

--- ui_controller.py
+++ ui_controller.py
@@ -1,29 +1,37 @@
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.lang import Builder
 from kivy.app import App
-from os import listdir
-import sys
-
-#add all the kv files in the current directory to the screenmanager
-for f in listdir("."):
-    if f.lower().endswith(".kv"):
-        Builder.load_file(f)
-
-class HomeScreen(Screen):
-    pass
-
-class JoinScreen(Screen):
-    pass
+from home_screen import HomeScreen
+from join_screen import JoinScreen
 
 sm = ScreenManager(transition=NoTransition())
-home = HomeScreen()
-join = JoinScreen()
+
+class Home(HomeScreen):
+    def __init__(self, **kwargs):
+        super(Home, self).__init__(**kwargs)
+        self.join_button.bind(on_release=switch_to_join)
+
+class Join(JoinScreen):
+    def __init__(self, **kwargs):
+        super(Join, self).__init__(**kwargs)
+        self.back_button.bind(on_release=switch_to_home)
+
+
+def switch_to_join(*args):
+    sm.current = 'join'
+
+def switch_to_home(*args):
+    sm.current = 'home'
+
+
+home = Home(name='home')
 sm.add_widget(home)
+
+join = Join(name='join')
 sm.add_widget(join)
 
 class MyApp(App):
     def build(self):
         return sm
 
-if __name__=='__main__':
+if __name__ == '__main__':
     MyApp().run()
