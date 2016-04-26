603f99b121cd645bdd83d0c5e4321ff1be085dde
sifusan <fuglestad.simen@gmail.com>
2016-04-26 17:04:51 +0200

    renamed ui-controller to main, import from subfolder tested and working

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class Home(HomeScreen):
         super(Home, self).__init__(**kwargs)
         self.join_button.bind(on_release=switch_to_join)
 
+        self.quit_button.bind(on_release=exit_application);
+
 class Join(JoinScreen):
     def __init__(self, **kwargs):
         super(Join, self).__init__(**kwargs)
@@ -22,6 +24,11 @@ def switch_to_join(*args):
 def switch_to_home(*args):
     sm.current = 'home'
 
+def exit_application(*args):
+    #This wrapper is necessary because storing exit(0) in on_release will
+    #exit the program immediatly after launch. Aslo, consider adding 
+    #more sophisticated error handling.
+    exit(0);
 
 home = Home(name='home')
 sm.add_widget(home)
