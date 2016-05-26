d664c4dcfa627ba8f88c09331e92968f9f241191
eanilsen <evenanilsen@gmail.com>
2016-05-26 10:41:10 +0200

    Working on error handling and such

--- src/util/filehandler.py
+++ src/util/filehandler.py
@@ -16,6 +16,16 @@ from kivy.uix.textinput import TextInput
 import kivy
 kivy.require('1.9.1')
 
+class TestFile(object):
+    def __init__(self):
+        self.name = "Test"
+        self.version = 420
+        self.is_dank = True
+
+    def test_print(self, string):
+        print string
+
+
 class FileHandler(BoxLayout):
     def __init__(self, **kwargs):
         super(FileHandler, self).__init__(**kwargs)
@@ -28,7 +38,8 @@ class FileHandler(BoxLayout):
         save_btn.bind(on_release=self.show_save_popup)
         self.add_widget(save_btn)
 
-        self.test = ["aoeu", 1]
+        self.test_file = TestFile()
+        self.test = []
 
 
     def show_load_popup(self, *args):
@@ -50,7 +61,8 @@ class FileHandler(BoxLayout):
     def show_save_popup(self, *args):
         text_input = TextInput(
             size_hint=(.20, .05),
-            pos_hint={'center_x': .5, 'bottom_y': .05})
+            pos_hint={'center_x': .5, 'bottom_y': .05},
+            multiline=False)
         save_btn = Button(text='Save', size_hint=(.08, .05))
         file_chooser = FileChooserIconView(path="../../saves")
 
@@ -69,30 +81,40 @@ class FileHandler(BoxLayout):
         popup.open()
 
     def load(self, path, filename, popup):
-        if platform.system() == "Linux":
-            try:
-                file_name = filename[0]
-                file_index = [m.start() for m in re.finditer('/', file_name)]
-                with open(os.path.join(path, file_name[file_index[-1]+1:]),
-                        'rb')as in_file:
-                    print self.deserialize(in_file.read())
-            except:
-                print "nope"
-        elif platform.system() == "Windows":
-            try:
-                file_name = filename[0]
-                file_index = [m.start() for m in re.finditer('\\', file_name)]
-                with open(os.path.join(path, file_name[file_index[-1]+1:]),
-                        'rb')as in_file:
-                    print self.deserialize(in_file.read())
-            except:
-                print "nope"
-        popup.dismiss()
+            if platform.system() == "Windows":
+                try:
+                    if '.ddh' not in filename[0]:
+                        print "Invalid file type."
+                    else:
+                        file_name = filename[0]
+                        file_index = [m.start() for m in re.finditer(r'\\', file_name)]
+                        with open(os.path.join(path, file_name[file_index[-1]+1:]),
+                                'rb')as in_file:
+                            print self.deserialize(in_file)
+                        popup.dismiss()
+                except:
+                    print "No file selected."
+            else:
+                try:
+                    if '.ddh' not in filename[0]:
+                        print "Invalid file type."
+                    else:
+                        file_name = filename[0]
+                        file_index = [m.start() for m in re.finditer('/', file_name)]
+                        with open(os.path.join(path, file_name[file_index[-1]+1:]),
+                                'rb')as in_file:
+                            test_object = self.deserialize(in_file)
+                        print "%s %d %s" % (test_object[0].name, test_object[0].version, test_object[0].is_dank)
+                        test_object[0].test_print("Vegard")
+                        popup.dismiss()
+                except:
+                    print "No file selected."
 
     def deserialize(self, file_string):
-        return pickle.loads(file_string)
+        return pickle.load(file_string)
 
     def serialize(self, path, out_file, popup):
+        self.test.append(self.test_file)
         with open(os.path.join(path, out_file + '.ddh'), 'wb') as of:
             pickle.dump(self.test, of)
         popup.dismiss()
